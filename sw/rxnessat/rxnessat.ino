ff1a53e0a8adf00038585b526c19c3342f56527a
Anthony Lieuallen <arantius@gmail.com>
2015-03-25 20:37:09 -0400

    Disable debug mode.

--- sw/rxnessat/rxnessat.ino
+++ sw/rxnessat/rxnessat.ino
@@ -8,7 +8,7 @@ This code is responsible for what needs to happen in the receiver:
 */
 
 // Define this to send debugging data out via Serial.
-#define DEBUG
+//#define DEBUG
 
 // Addresses of data stored in EEPROM.
 #define EEPROM_RF_CHAN 0x00 /* One byte, the RF channel. */
