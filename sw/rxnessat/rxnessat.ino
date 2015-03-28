e998845b2c8096ef6eb10e81362901945afa378a
Anthony Lieuallen <arantius@gmail.com>
2015-03-28 16:19:57 -0400

    Working NES receiver software.

--- sw/rxnessat/rxnessat.ino
+++ sw/rxnessat/rxnessat.ino
@@ -8,7 +8,7 @@ This code is responsible for what needs to happen in the receiver:
 */
 
 // Define this to send debugging data out via Serial.
-//#define DEBUG
+#define DEBUG
 
 // Addresses of data stored in EEPROM.
 #define EEPROM_RF_CHAN 0x00 /* One byte, the RF channel. */
@@ -21,7 +21,6 @@ This code is responsible for what needs to happen in the receiver:
 
 #include <EEPROM.h>
 #include <SPI.h>
-#include <util/atomic.h>
 
 #include "nRF24L01.h"
 #include "RF24.h"
@@ -43,7 +42,6 @@ This code is responsible for what needs to happen in the receiver:
 #define PORT_CLK2 PORTD
 #define PIN_CLK2 _BV(6)
 
-
 #define PORT_D0P1 PORTC
 #define PIN_D0P1 _BV(4)
 
@@ -64,23 +62,16 @@ This code is responsible for what needs to happen in the receiver:
 
 // \\ // \\ // \\ // \\ // \\ // \\ // \\ // \\ // \\ // \\ // \\ // \\ // \\ //
 
-enum LedState {
-  OFF,
-  GREEN_SOLID,
-  GREEN_BLINK_ON,
-  GREEN_BLINK_OFF,
-  YELLOW_SOLID,
-  YELLOW_BLINK_ON,
-  YELLOW_BLINK_OFF
-};
 
 RF24 radio(10, 14);  // CE, CS pins
-
-LedState gLedState = YELLOW_BLINK_ON;
 unsigned long gLastDataTime = 0;
-unsigned long gNextLedBlinkTime = 0;
+volatile uint8_t gData = 0xFF;
 
-volatile uint8_t gData = 0xFFFF;
+// \\ // \\ // \\ // \\ // \\ // \\ // \\ // \\ // \\ // \\ // \\ // \\ // \\ //
+
+#define lightLedOff() PORT_GRN &= ~PIN_GRN; PORT_YEL &= ~PIN_YEL;
+#define lightLedGrn() PORT_YEL &= ~PIN_YEL; PORT_GRN |= PIN_GRN;
+#define lightLedYel() PORT_GRN &= ~PIN_GRN; PORT_YEL |= PIN_YEL;
 
 // \\ // \\ // \\ // \\ // \\ // \\ // \\ // \\ // \\ // \\ // \\ // \\ // \\ //
 
@@ -94,7 +85,7 @@ void ISR_latch1() {
 
   for (uint8_t i = 0; i < 8; i++) {
     // Console is reading data now.  Wait for clock to rise.
-    while ( !(PINC & PIN_CLK1) ) { }
+    while (!(PINC & PIN_CLK1)) ;;
 
     // Set data pin for this bit.
     if (data & 0x01) {
@@ -105,51 +96,21 @@ void ISR_latch1() {
     // Shift in next LSB of data.
     data = data >> 1;
 
-    // Wait for clock to drop.  (Console will read DATA when CLOCK drops.)
-    while (PINC & PIN_CLK1) { }
+    // Wait for clock to drop.  The NES sends a low-going clock pulse only
+    // about a half a microsecond wide.  Limit how long to wait for it, as
+    // otherwise we rarely miss it, and end up blocking forever for a clock
+    // pulse that doesn't come.  But, unroll the loop so that we don't spend so
+    // much time processing the loop itself that we regularly miss the clock.
+    for (uint8_t j = 0; j < 48; j++) {
+      if (!(PINC & PIN_CLK1)) break;
+      if (!(PINC & PIN_CLK1)) break;
+      if (!(PINC & PIN_CLK1)) break;
+      if (!(PINC & PIN_CLK1)) break;
+    }
   }
 
-  // Force pin high for the rest of the cycle.  These bits aren't used for
-  // normal controllers.
+  // Force data high until the next cycle.
   PORT_D0P1 |= PIN_D0P1;
-}
-
-// \\ // \\ // \\ // \\ // \\ // \\ // \\ // \\ // \\ // \\ // \\ // \\ // \\ //
-
-void lightLed(int ledState) {
-  unsigned long now = millis();
-  boolean switchBlink = now > gNextLedBlinkTime;
-  if (switchBlink) {
-    gNextLedBlinkTime = now + BLINK_INTERVAL;
-  }
-
-  // Set both off.
-  PORT_GRN &= ~PIN_GRN;
-  PORT_YEL &= ~PIN_YEL;
-  // Then just the right one on.
-  switch (ledState) {
-  case GREEN_BLINK_OFF:
-    if (switchBlink) gLedState = GREEN_BLINK_ON;
-    break;
-  case YELLOW_BLINK_OFF:
-    if (switchBlink) gLedState = YELLOW_BLINK_ON;
-    break;
-  case GREEN_BLINK_ON:
-    if (switchBlink) gLedState = GREEN_BLINK_OFF;
-  case GREEN_SOLID:
-    PORT_GRN |= PIN_GRN;
-    break;
-  case YELLOW_BLINK_ON:
-    if (switchBlink) gLedState = YELLOW_BLINK_OFF;
-  case YELLOW_SOLID:
-    PORT_YEL |= PIN_YEL;
-    break;
-  }
-}
-
-
-void lightLed() {
-  lightLed(gLedState);
 }
 
 // \\ // \\ // \\ // \\ // \\ // \\ // \\ // \\ // \\ // \\ // \\ // \\ // \\ //
@@ -158,23 +119,22 @@ void loop(void) {
   unsigned long now = millis();
 
   if (gLastDataTime && (gLastDataTime + DATA_MIN_INTERVAL > now)) {
-    gLedState = GREEN_SOLID;
+    lightLedGrn();
   } else {
-    gLedState = YELLOW_SOLID;
+    lightLedYel();
   }
-  lightLed();
 
   // TODO: Set appropriate masks and just read RF_IRQ pin to see pending data?
-  if (radio.available()) {
-    // Read data from the radio.
-    uint16_t data_raw;
+  uint16_t data_raw=0;
+  while (radio.available()) {
+    radio.read(&data_raw, sizeof(data_raw));
+  }
+
+  if (data_raw) {
     // TODO: Read multiple controllers.
-    while (radio.available()) {
-      radio.read(&data_raw, sizeof(data_raw));
-    }
     // TODO: Use battery/sleeping status bits?
 
-    // Translate air data to NES data.
+    // Translate RF data to NES data.
     uint8_t data_nes = 0x00;
     data_nes = data_nes << 1; if (data_raw & _BV(7)) data_nes |= 1; // Right
     data_nes = data_nes << 1; if (data_raw & _BV(6)) data_nes |= 1; // Left
@@ -184,15 +144,11 @@ void loop(void) {
     data_nes = data_nes << 1; if (data_raw & _BV(2)) data_nes |= 1; // Select
     data_nes = data_nes << 1; if (data_raw & _BV(0)) data_nes |= 1; // B
     data_nes = data_nes << 1; if (data_raw & _BV(8)) data_nes |= 1; // A
-
     #ifdef DEBUG
     printf("RX got %04x; Translt. %02x\n", data_raw, data_nes);
     #endif
 
-    ATOMIC_BLOCK(ATOMIC_RESTORESTATE) {
-      gData = data_nes;
-    }
-
+    gData = data_nes;
     gLastDataTime = now;
   }
 }
@@ -227,8 +183,8 @@ void setup() {
   DDRB |= PIN_GRN;
   DDRD |= PIN_YEL;
   // Pins D0_1, D0_2, D3_2, D4_2.
-  DDRC |= _BV(1) | _BV(4);
-  DDRD |= _BV(4) | _BV(5);
+  DDRC |= PIN_D0P1 | PIN_D3P2;
+  DDRD |= PIN_D0P2 | PIN_D4P2;
 
   // The CLOCK and LATCH signals should be pullup enabled inputs.
   DDRC &= ~PIN_CLK1;
@@ -240,6 +196,7 @@ void setup() {
   PORT_D0P1 |= PIN_D0P1;
   // DATA output is fully interrupt driven, from LATCH.
   attachInterrupt(0, ISR_latch1, RISING);
+  // TODO: Handle players 2 through 4.
 
   #ifdef DEBUG
   printf("\n");
